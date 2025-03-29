8326be3b5da5c6cda6c75e2ff99b7536e6b7d524
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2025-03-29 09:50:07 +0800

    added react js frontend and tailwind

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     'rest_framework',
+    'corsheaders',
 
     'pronovetai_app',
 ]
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
