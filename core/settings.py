a1b6c500b87abf3fca4b26d0178e7c316a3ea576
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2025-04-01 08:45:40 +0800

    create requirement.txt and modify settings for backend

--- core/settings.py
+++ core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
 
     'pronovetai_app',
 ]
@@ -64,6 +65,12 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = "core.urls"
--- pronovetai_frontend/src/components/StaffRegistration.js
+++ pronovetai_frontend/src/components/StaffRegistration.js
@@ -27,7 +27,7 @@ const StaffRegistration = () => {
         }
 
         try {
-            const response = await fetch('http://localhost:8080/api/register/staff', {
+            const response = await fetch('http://127.0.0.1:8000/api/register/staff', {
                 method: 'POST',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify(formData)
