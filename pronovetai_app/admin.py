bff16101165dd4b597223d2d7b0cc87403ccc3fb
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2025-04-01 14:55:35 +0800

    added models to the backend

--- pronovetai_app/admin.py
+++ pronovetai_app/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
+from .models import User, Company, Building, Unit, ODForm, Address, Contact
 
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Company)
+admin.site.register(Building)
+admin.site.register(Unit)
+admin.site.register(ODForm)
+admin.site.register(Address)
+admin.site.register(Contact)
