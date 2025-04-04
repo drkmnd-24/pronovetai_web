9e5f2f42a581c4f5beb7b1741f2ba1bfd2966b90
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2025-04-04 10:30:02 +0800

    update Login button

--- pronovetai_frontend/src/components/Login.js
+++ pronovetai_frontend/src/components/Login.js
@@ -71,7 +71,7 @@ const Login = () => {
             type="submit"
             className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
           >
-            Login
+            Login to Pronove TAI
           </button>
         </form>
         <div className="mt-4 text-center">
