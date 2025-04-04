c3171eb6e263e58d82c8bd6b4ddee1a410ad14b6
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2025-04-04 09:33:31 +0800

    update app routing

--- pronovetai_frontend/src/App.js
+++ pronovetai_frontend/src/App.js
@@ -1,25 +1,35 @@
+// src/App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import StaffRegistration from "./components/StaffRegistration";
 import ManagerRegistration from "./components/ManagerRegistration";
-import Dashboard from "./components/Dashboard";
+import Dashboard from './components/Dashboard';
+import BuildingsList from "./components/BuildingsList";
+import UnitsList from "./components/UnitsList";
+import CompaniesList from "./components/CompaniesList";
+import ContactsList from "./components/ContactsList";
+import ODFormsList from "./components/ODFormsList";
 import './App.css';
 
-
 function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/login" element={<Login />} />
-                <Route path="/register/staff" element={<StaffRegistration />} />
-                <Route path="/register/manager" element={<ManagerRegistration />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                {/* Default route */}
-                <Route path="*" element={<Login />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register/staff" element={<StaffRegistration />} />
+        <Route path="/register/manager" element={<ManagerRegistration />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/list/buildings" element={<BuildingsList />} />
+        <Route path="/list/units" element={<UnitsList />} />
+        <Route path="/list/companies" element={<CompaniesList />} />
+        <Route path="/list/contacts" element={<ContactsList />} />
+        <Route path="/list/odforms" element={<ODFormsList />} />
+        {/* Default route */}
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
