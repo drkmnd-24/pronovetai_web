1635307f1a0bf6669fdcb08f638f1f7529db4cb5
Vanj Mampusti <vanjo.mampusti0324@gmail.com>
2025-05-18 17:57:40 +0800

    refactor serializer

--- pronovetai_app/serializers.py
+++ pronovetai_app/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
-
 from django.contrib.auth import password_validation
+from .models import (
+    UserType,
+    User,
+    UserLog,
+    Address,
+    Company,
+    Contact,
+    Building,
+    Unit,
+    ODForm,
+    BuildingImage,
+    UnitImage,
+)
 
-from .models import (Address, User, Company,
-                     Contact, Building, Unit,
-                     ODForm, BuildingImage, UnitImage, UserLog)
+
+class UserTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserType
+        fields = ['id', 'description']
 
 
 class StaffRegistrationSerializer(serializers.ModelSerializer):
@@ -13,24 +27,28 @@ class StaffRegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
+        # we no longer have a 'role' field; we'll hard‐code user_type
         fields = ['username', 'email', 'first_name', 'last_name', 'password', 'confirm_password']
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError('Password do not match')
+            raise serializers.ValidationError("Passwords do not match")
         return data
 
     def create(self, validated_data):
         validated_data.pop('confirm_password')
+        # pick the UserType row whose description is “User”
+        user_type = UserType.objects.get(description__iexact='User')
         user = User(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email', ''),
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
-            role='user'
+            user_type=user_type,
         )
         user.set_password(validated_data['password'])
-        user.is_staff = True
+        # no DB column, but Django/auth will read this flag in memory:
+        user.is_staff = False
         user.save()
         return user
 
@@ -45,17 +63,18 @@ class ManagerRegistrationSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError('Password do not match')
+            raise serializers.ValidationError("Passwords do not match")
         return data
 
     def create(self, validated_data):
         validated_data.pop('confirm_password')
+        user_type = UserType.objects.get(description__iexact='Manager')
         user = User(
             username=validated_data['username'],
-            email=validated_data['email'],
+            email=validated_data.get('email', ''),
             first_name=validated_data.get('first_name', ''),
             last_name=validated_data.get('last_name', ''),
-            role='manager'
+            user_type=user_type,
         )
         user.set_password(validated_data['password'])
         user.is_staff = True
@@ -70,9 +89,11 @@ class AddressSerializer(serializers.ModelSerializer):
 
 
 class UserSerializer(serializers.ModelSerializer):
+    user_type = UserTypeSerializer(read_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'date_joined', 'user_type']
 
 
 class UserLogSerializer(serializers.ModelSerializer):
@@ -81,7 +102,7 @@ class UserLogSerializer(serializers.ModelSerializer):
         fields = ['id', 'message', 'timestamp']
 
 
-class ChangePasswordSerializer(serializers.ModelSerializer):
+class ChangePasswordSerializer(serializers.Serializer):
     new_password = serializers.CharField(write_only=True, required=True)
 
     def validate_new_password(self, value):
@@ -104,20 +125,19 @@ class CompanySerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         address_data = validated_data.pop('address', None)
         if address_data:
-            address = Address.objects.create(**address_data)
-            validated_data['address'] = address
+            addr = Address.objects.create(**address_data)
+            validated_data['address'] = addr
         return Company.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         address_data = validated_data.pop('address', None)
-        if address_data and instance.address:
-            address = instance.address
-            for key, value in address_data.items():
-                setattr(address, key, value)
-            address.save()
-        elif address_data:
-            address = Address.objects.create(**address_data)
-            instance.address = address
+        if address_data:
+            if instance.address:
+                for k, v in address_data.items():
+                    setattr(instance.address, k, v)
+                instance.address.save()
+            else:
+                instance.address = Address.objects.create(**address_data)
         return super().update(instance, validated_data)
 
 
@@ -135,22 +155,21 @@ class BuildingSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def create(self, validated_data):
-        address_data = validated_data.pop('address', None)
-        if address_data:
-            address = Address.objects.create(**address_data)
-            validated_data['address'] = address
+        addr_data = validated_data.pop('address', None)
+        if addr_data:
+            addr = Address.objects.create(**addr_data)
+            validated_data['address'] = addr
         return Building.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        address_data = validated_data.pop('address', None)
-        if address_data and instance.address:
-            address = instance.address
-            for key, value in address_data.items():
-                setattr(address, key, value)
-            address.save()
-        elif address_data:
-            address = Address.objects.create(**address_data)
-            instance.address = address
+        addr_data = validated_data.pop('address', None)
+        if addr_data:
+            if instance.address:
+                for k, v in addr_data.items():
+                    setattr(instance.address, k, v)
+                instance.address.save()
+            else:
+                instance.address = Address.objects.create(**addr_data)
         return super().update(instance, validated_data)
 
 
@@ -160,9 +179,8 @@ class UnitSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, data):
-        # Invoke model clean() for custom validations.
-        instance = Unit(**data)
-        instance.clean()
+        # trigger model.clean()
+        Unit(**data).clean()
         return data
 
 
@@ -172,9 +190,7 @@ class ODFormSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def validate(self, data):
-        # Invoke model clean() for custom validations.
-        instance = ODForm(**data)
-        instance.clean()
+        ODForm(**data).clean()
         return data
 
 
